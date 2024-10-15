3a1b46bf30974f5da0acb1b20c5d884b8113bff7
Stamili <96841958+Stamili@users.noreply.github.com>
2024-10-15 16:47:14 +0330

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let result = Math.floor(Math.random() * 3)
+    if (result === 0) {
+        console.log("Rock")
+    } else if (result === 1) {
+        console.log("Paper")
+    } else if (result === 2) {
+        console.log("Scissors");
+    }
+}
