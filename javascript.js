13392cfa6af81220f03600341749f88c9ac88cbf
Stamili <96841958+Stamili@users.noreply.github.com>
2024-10-18 21:42:40 +0330

    Add getHumanChoice function
    And fixed getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,27 @@
+let humanScore = 0
+let computerScore = 0
+
+
 function getComputerChoice() {
     let result = Math.floor(Math.random() * 3)
     if (result === 0) {
-        console.log("Rock")
+        return "rock";
     } else if (result === 1) {
-        console.log("Paper")
+        return "paper";
     } else if (result === 2) {
-        console.log("Scissors");
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Your choice?").toLowerCase()
+    if (humanChoice === "rock") {
+        return humanChoice
+    } else if (humanChoice === "paper") {
+        return humanChoice
+    } else if (humanChoice === "scissors") {
+        return humanChoice
+    } else {
+        return "Not a valid input :("
     }
 }
