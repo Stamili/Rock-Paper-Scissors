19cca5060fdebc619d53d1d9f4a4a66887c6ba77
Stamili <96841958+Stamili@users.noreply.github.com>
2025-02-02 15:12:40 +0330

    Add scores

--- javascript.js
+++ javascript.js
@@ -1,79 +1,85 @@
-console.log("Welcome!");
-
 let playerScore = 0;    
 let computerScore = 0;
 
+let computerChoice = "";
+let playerChoice = "";
+
 function getComputerChoice() {
     let result = Math.floor(Math.random() * 3);
-    let computerChoice;
     if (result === 0) {
-    computerChoice = "rock";
+    computerChoice = "Rock";
     } 
     else if (result === 1) {
-    computerChoice = "paper";
+    computerChoice = "Paper";
     } 
     else if (result === 2) {
-    computerChoice = "scissors";
+    computerChoice = "Scissors";
     }
     return computerChoice;
 }
 
 const buttons = document.querySelector("#buttons");
 buttons.addEventListener('click', function(e) {
-    let playerSelection;
-    let computerSelection = getComputerChoice();
+    computerChoice = getComputerChoice();
 
     switch(e.target.className) {
         case("R"):
-        playerSelection = "rock";
+        playerChoice = "Rock";
         break;
         case("P"):
-        playerSelection = "paper";
+        playerChoice = "Paper";
         break;
         case("S"):
-        playerSelection = "scissors";
+        playerChoice = "Scissors";
     }
     
-    playRound(playerSelection, computerSelection);
+    playRound(playerChoice, computerChoice);
+    choices.querySelector(".compChoice").textContent = "Computer choice: " + computerChoice;
+    choices.querySelector(".playerChoice").textContent = "Your choice: " + playerChoice; 
+    points.querySelector(".comp").textContent = "Computer: " + computerScore + "/5";
+    points.querySelector(".player").textContent = "You: " + playerScore + "/5";
 });
 
 
+const results = document.querySelector("#results");
+const points = document.querySelector("#points");
+const choices = document.querySelector("#choices");
 
+choices.querySelector(".compChoice").textContent = "Computer choice: " + computerChoice;
+choices.querySelector(".playerChoice").textContent = "Your choice: " + playerChoice; 
 
-
+points.querySelector(".comp").textContent = "Computer: " + computerScore + "/5";
+points.querySelector(".player").textContent = "You: " + playerScore + "/5";
 
 function playRound(playerChoice, computerChoice) {
-    console.log("You : " + playerChoice);
-    console.log("Computer : " + computerChoice);
-    if (playerChoice === "paper" && computerChoice === "rock") {
-        playerScore++;
-        console.log("You won! paper beats rock!");
-    }
-    else if (playerChoice === "paper" && computerChoice === "scissors") {
-        computerScore++;
-        console.log("You lost, scissors beats paper");
-    }
-    else if (playerChoice === "rock" && computerChoice === "paper") {
-        computerScore++;
-        console.log("You lost, paper beats rock!");
-    }
-    else if (playerChoice === "rock" && computerChoice === "scissors") {
-        playerScore++;
-        console.log("You won! rock beats scissors!");
-    }
-    else if (playerChoice === "scissors" && computerChoice === "rock") {
-        computerScore++;
-        console.log("You lost, rock beats scissors!");
-    }
-    else if (playerChoice === "scissors" && computerChoice === "paper") {
-        playerScore++;
-        console.log("You won! scissors beats paper");
-    }
-    else if (playerChoice === computerChoice) {
-        console.log("It's a tie!");
-    }
-    else {
-        console.log("Something went wrong...");
+    if (playerScore < 5 && computerScore < 5) {
+        if (playerChoice === "Paper" && computerChoice === "Rock") {
+            playerScore++;
+            results.textContent = "You Win!";
+        } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+            computerScore++;
+            results.textContent = "You lose!";
+        } else if (playerChoice === "Rock" && computerChoice === "Paper") {
+            computerScore++;
+            results.textContent = "You lose!";
+        } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+            playerScore++;
+            results.textContent = "You Win!";
+        } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+            computerScore++;
+            results.textContent = "You lose!";
+        } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+            playerScore++;
+            results.textContent = "You Win!";
+        } else if (playerChoice === computerChoice) {
+            results.textContent = "It's a tie!";
+        }
+
+        if (playerScore === 5) {
+            results.textContent = "Congratulations! You won the game.";
+        } else if (computerScore === 5) {
+            results.textContent = "Oh! You lost the game :\\";
+        }
     }
 }
 
